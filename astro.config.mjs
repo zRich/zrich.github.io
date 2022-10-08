848e151e3f3d06915d95d8b72f16734b8ae04db3
Rich Zhao <zhao.zhenhua@gmail.com>
2022-10-08 23:24:18 +0800

    react & tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zrich.github.io',
   base: '/',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind(), react(), image()]
 });
