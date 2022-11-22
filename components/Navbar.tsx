0850a7f9a762ac3a99b824f225645fc2610a3f9f
Rich Zhao <zhao.zhenhua@gmail.com>
2022-11-22 20:29:14 +0800

    add components

---
+++ components/Navbar.tsx
@@ -0,0 +1,141 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
+import { GoMarkGithub } from "react-icons/go";
+import SiteLogo from "/public/zhao.jpg";
+import WechatIcon from "/public/wechat-icon.png";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="relative overflow-x-hidden">
+      <div className="relative my-4 flex justify-between items-center px-8">
+        <Link href="/">
+          <Image
+            src={SiteLogo}
+            alt="/"
+            width="36"
+            height="36"
+            className="cursor-pointer rounded-full"
+          />
+        </Link>
+        <div>
+          <ul className="hidden md:flex">
+            <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/posts">Blog</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+          {/* Hamburger Icon */}
+          <div onClick={handleNav} className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      <div
+        className={nav ? "md:hidden fixed left-0 top-0 w-full bg-black/70" : ""}
+      >
+        {/* Side Drawer Menu */}
+        <div
+          className={
+            nav
+              ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] bg-slate-100 p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Link href="/">
+                <Image
+                  src={SiteLogo}
+                  width="60"
+                  height="60"
+                  alt="/"
+                  className="rounded-full"
+                />
+              </Link>
+              <div
+                onClick={handleNav}
+                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+              >
+                <AiOutlineClose />
+              </div>
+            </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Home
+                </li>
+              </Link>
+              <Link href="/posts">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Blog
+                </li>
+              </Link>
+              <Link href="/about">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  About
+                </li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-blue-600 font-bold">
+                Contact
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <a
+                  href="https://github.com/zRich"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <GoMarkGithub />
+                  </div>
+                </a>
+                <Link href="/#contact">
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300"
+                  >
+                    <AiOutlineMail />
+                  </div>
+                </Link>
+                <Link href="/resume">
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300"
+                  >
+                    <Image
+                      src={WechatIcon}
+                      alt="/"
+                      width="16"
+                      height="16"
+                      className="cursor-pointer"
+                    />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
