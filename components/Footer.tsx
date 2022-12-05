4e14347692ac7eeeb3ce2f339e24425dd808b3f9
Rich Zhao <zhao.zhenhua@gmail.com>
2022-12-05 11:22:30 +0800

    web3-profit-model

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,16 +5,18 @@ export default function Footer() {
   return (
     <div>
       <div className='flex flex-col justify-between items-center'>
-        <div className='flex flex-row md:w-[50%] justify-between items-center '>
-          <div className='font-bold gap-4'>
-            <p>Telegram: x0rich</p>
+        <div className='flex flex-row max-w-[50%] justify-between items-center gap-4 '>
+          <div className='flex flex-col justify-center items-center'>
+            <p className='text-xs'>电报</p>
+            <Image src='telegram.jpg' height={40} width={40} alt='telegram' />
           </div>
-          <div>
-            <Image src='wechat.jpg' height={50} width={50} alt='Wechat' />
+          <div className='flex flex-col justify-center items-center'>
+            <p className='text-xs'>微信</p>
+            <Image src='wechat.jpg' height={40} width={40} alt='Wechat' />
           </div>
         </div>
-        <div className='font-bold justify-center items-center'>
-          <p>Copyright Zhao Zhenhua 2022</p>
+        <div className='font-bold justify-center items-center py-4'>
+          <p>Copyright &copy; Zhao Zhenhua 2022</p>
         </div>
       </div>
     </div>
